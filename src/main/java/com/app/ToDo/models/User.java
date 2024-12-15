018f43cb5572cd5e30d7971624e7c7b278220522
Gabi <playcs.gabi@yahoo.com>
2024-12-15 22:02:22 +0200

    Added some attributes for User.

--- src/main/java/com/app/ToDo/models/User.java
+++ src/main/java/com/app/ToDo/models/User.java
@@ -1,4 +1,27 @@
 package com.app.ToDo.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private Date dateOfBirth;
+    private LocalDate registrationDate;
+    private String email;
+    private String userName;
+    private String password;
 }
