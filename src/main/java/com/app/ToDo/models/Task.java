651d5c2fbd9676fe29063f915445ba5c858130b7
Gabi <playcs.gabi@yahoo.com>
2024-12-15 19:23:58 +0200

    i make Task atributes

--- src/main/java/com/app/ToDo/models/Task.java
+++ src/main/java/com/app/ToDo/models/Task.java
@@ -1,4 +1,34 @@
 package com.app.ToDo.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+    private String description;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
