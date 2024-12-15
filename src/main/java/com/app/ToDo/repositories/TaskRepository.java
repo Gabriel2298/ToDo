3e2f2f6e5647af698349da0918103248f5955bb9
Gabi <playcs.gabi@yahoo.com>
2024-12-15 19:30:42 +0200

    Make extension to JpaRepository with parameter task and long.

--- src/main/java/com/app/ToDo/repositories/TaskRepository.java
+++ src/main/java/com/app/ToDo/repositories/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.app.ToDo.repositories;
 
-public interface  TaskRepository {
+import com.app.ToDo.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  TaskRepository extends JpaRepository<Task, Long> {
 }
