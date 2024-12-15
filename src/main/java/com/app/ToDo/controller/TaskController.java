bfcfb373a88d6b83cca0a92a11b90ac5e7b4feae
Gabi <playcs.gabi@yahoo.com>
2024-12-15 19:44:24 +0200

    I eliminate RequestMapping.

--- src/main/java/com/app/ToDo/controller/TaskController.java
+++ src/main/java/com/app/ToDo/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/tasks")
+@
 public class TaskController {
     private final TaskService taskService;
 
