aa813af0f793a2f6d009455cf295407358b8b318
Gabi <playcs.gabi@yahoo.com>
2024-12-24 20:10:13 +0200

    I add delete, get , get all and login methods for UserService

--- src/main/java/com/app/ToDo/service/UserService.java
+++ src/main/java/com/app/ToDo/service/UserService.java
@@ -1,12 +1,21 @@
 package com.app.ToDo.service;
 
-import com.app.ToDo.dtos.CreateUserDto;
+import com.app.ToDo.dtos.UserDto;
+
+import java.util.List;
 
 public interface UserService{
 
     // create users
-    CreateUserDto createUser(CreateUserDto createUserDto);
-
+    UserDto createUser(UserDto userDto);
     // update users
-    CreateUserDto updateUser( CreateUserDto createUserDto, Long userId);
+    UserDto updateUser(UserDto userDto, Long userId);
+    // delete users
+    void deletedUser(Long userId);
+    // get users
+    UserDto getUser(Long userId);
+    //get all users
+    List<UserDto> getAllUsers();
+    // user login
+    UserDto userLogin(String email, String password);
 }
