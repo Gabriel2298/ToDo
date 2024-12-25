c42bdf930167769d18337c7dc02c7538939c3071
Gabi <playcs.gabi@yahoo.com>
2024-12-25 14:19:00 +0200

    Add documentation for all methods!

--- src/main/java/com/app/ToDo/controller/TaskController.java
+++ src/main/java/com/app/ToDo/controller/TaskController.java
@@ -3,14 +3,13 @@ package com.app.ToDo.controller;
 import com.app.ToDo.models.Task;
 import com.app.ToDo.service.impl.TaskServiceImpl;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
- * test test
+ * Controller class for handling tasks.
  */
 @RestController
 public class TaskController {
@@ -21,6 +20,12 @@ public class TaskController {
         this.taskServiceImpl = taskServiceImpl;
     }
 
+    /**
+     * Retrieves all tasks and adds them to the Model for rendering on the view.
+     *
+     * @param model the Model object to which the tasks are added
+     * @return the view name for displaying the tasks
+     */
     @GetMapping
     public String getTasks(@NotNull Model model) {
         List<Task> tasks = taskServiceImpl.getAllTasks();
@@ -28,18 +33,38 @@ public class TaskController {
         return "tasks";
     }
 
+    /**
+     * Creates a new task with the given title, description, and completion status.
+     *
+     * @param title the title of the task
+     * @param description the description of the task
+     * @param completed true if the task is already completed, false otherwise
+     * @return a redirection to the home page "/"
+     */
     @PostMapping("/tasks")
     public String createTask(String title, String description, boolean completed) {
         taskServiceImpl.createTask(title,description, completed);
         return "redirect:/";
     }
 
+    /**
+     * Deletes a task with the specified ID.
+     *
+     * @param id the ID of the task to be deleted
+     * @return a redirection to the home page "/"
+     */
     @GetMapping("{id}/delete")
     public String deleteTask(@PathVariable Long id) {
         taskServiceImpl.deleteTask(id);
         return "redirect:/";
     }
 
+    /**
+     * Toggles the completion status of a task with the specified ID.
+     *
+     * @param id the ID of the task to toggle
+     * @return a redirection to the home page "/"
+     */
     @GetMapping("/{id}/toggle")
     public String toggleTasks(@PathVariable Long id) {
         taskServiceImpl.toggleTask(id);
