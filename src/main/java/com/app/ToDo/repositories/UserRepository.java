b664722746cb5caa7883c82057bab698c40c967b
Gabi <playcs.gabi@yahoo.com>
2024-12-26 17:20:48 +0200

    I change in jpaRepository, and I  make 2 methods!

--- src/main/java/com/app/ToDo/repositories/UserRepository.java
+++ src/main/java/com/app/ToDo/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.app.ToDo.repositories;
 
 import com.app.ToDo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByEmailAndPassword(String email, String password);
 }
