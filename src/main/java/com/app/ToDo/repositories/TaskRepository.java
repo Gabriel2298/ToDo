82d796f837b10068cee79f59615f7f0cbba77cef
Gabi <playcs.gabi@yahoo.com>
2024-12-26 17:21:20 +0200

    I add i methods!

--- src/main/java/com/app/ToDo/repositories/TaskRepository.java
+++ src/main/java/com/app/ToDo/repositories/TaskRepository.java
@@ -1,10 +1,14 @@
 package com.app.ToDo.repositories;
 
 import com.app.ToDo.models.Task;
+import com.app.ToDo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface  TaskRepository extends JpaRepository<Task, Long> {
+List<Task> findAllByUser(User user);
 
 }
