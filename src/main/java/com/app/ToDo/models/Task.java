13b10a95a9223d8dec1c78589f494308a54c2d35
Gabi <playcs.gabi@yahoo.com>
2024-12-26 17:09:21 +0200

    I add annotation for getter, setter, Data and 2 more, and i make some modify to the attributes!

--- src/main/java/com/app/ToDo/models/Task.java
+++ src/main/java/com/app/ToDo/models/Task.java
@@ -1,64 +1,30 @@
 package com.app.ToDo.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
+
+import java.util.Date;
+
 @Entity
-@Table(name = "Tasks")
+@Table(name = "tasks")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 1000)
     private String description;
 
-    private boolean completed;
     @ManyToOne
-    @JoinColumn(name = "user_id")
     private User user;
-
-    public Task() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date;
 }
