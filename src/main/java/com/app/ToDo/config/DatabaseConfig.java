e0aae811fc3cd3f8629bcad1d5c72ec07c64caba
Gabi <playcs.gabi@yahoo.com>
2024-12-29 13:46:23 +0200

    I add annotation primary!

--- src/main/java/com/app/ToDo/config/DatabaseConfig.java
+++ src/main/java/com/app/ToDo/config/DatabaseConfig.java
@@ -2,6 +2,7 @@ package com.app.ToDo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -9,6 +10,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
     @Bean
+    @Primary
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
